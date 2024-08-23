e3eeb1e24b90898815385ecb185262aab5837eb5
salinasmaury <134342603+salinasmaury@users.noreply.github.com>
2024-08-23 18:04:00 -0300

    inputs y estilos para el layout de galeria

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Acá toda la lógica para el index
+/*
 let descripcion = document.getElementById("productoTextoNr1");
 let productos = [{nombre:"remera",precio:2400},{nombre:"Camisa",precio:300}]
-descripcion.innerText = `${productos[0].nombre} Precio: ${productos[0].precio}`
+descripcion.innerText = `${productos[0].nombre} Precio: ${productos[0].precio}` */ 

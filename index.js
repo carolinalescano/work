8be248c5c1f1330934b97ef0271dc0f02d7f84b1
salinasmaury <134342603+salinasmaury@users.noreply.github.com>
2024-08-26 23:21:55 -0300

    casi casi

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Acá toda la lógica para el index
+let descripcion = document.getElementById("productoTextoNr1");
+let productos = [{nombre:"remera",precio:2400},{nombre:"Camisa",precio:300}]
+descripcion.innerText = `${productos[0].nombre} Precio: ${productos[0].precio}`
